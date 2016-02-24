0103d3680cec2726e81d1c844785009f39da77cb
nfactorial <nfactorial@btopenworld.com>
2016-02-24 18:28:17 +0000

    More documentation updates.

--- imagegen/image_block.py
+++ imagegen/image_block.py
@@ -12,6 +12,38 @@ class ImageBlock:
         self.pos = pos
         self.size = size
 
+    @property
+    def x(self):
+        """
+        Gets the position along the horizontal axis of the image block object.
+        :return: The position along the horizontal axis of the image block.
+        """
+        return self.pos[0]
+
+    @property
+    def y(self):
+        """
+        Gets the position along the vertical axis of the image block object.
+        :return: The position along the vertical axis of the image block.
+        """
+        return self.pos[1]
+
+    @property
+    def width(self):
+        """
+        Gets the width (in pixels) of the image block object.
+        :return: The width (in pixels) of the image block.
+        """
+        return self.size[0]
+
+    @property
+    def height(self):
+        """
+        Gets the height (in pixels) of the image block object.
+        :return: The height (in pixels) of the image block object.
+        """
+        return self.size[1]
+
     def next_pixel(self):
         """
         Generator function that returns the coordinates of each pixel within the ImageBlock object.
