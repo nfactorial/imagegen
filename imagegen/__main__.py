7c76e4720cfbe8eee0470a6fab268ad68df23147
nfactorial <nfactorial@btopenworld.com>
2016-02-29 15:47:42 +0000

    Minor improvements to main.py, user may now specify job size from the command line.

--- imagegen/__main__.py
+++ imagegen/__main__.py
@@ -37,31 +37,39 @@ parser.add_argument('-f',
 parser.add_argument('--nodes',
                     action='store_true',
                     help='List all the nodes available within the imagegen distribution.')
+parser.add_argument('-job',
+                    type=int,
+                    help='The number of pixels to processed by each job.')
 args = parser.parse_args()
 
+DEFAULT_JOB_SIZE = 32
+
+# List all nodes if the user requested it within the command line.
 if args.nodes:
     print('Available nodes: ')
     for key, value in NODE_REGISTRY.items():
         if value.description:
-            print('\t' + key + ' - ' + value.description)
+            print('\t%s - %s' % (key, value.description))
         else:
-            print('\t' + key + ' - <no description>')
+            print('\t%s - <no description>' % key)
+
+JOB_SIZE = DEFAULT_JOB_SIZE if args.job is None else args.job
+if JOB_SIZE <= 0:
+    raise ValueError('Job size must be greater than zero (found %d).' % JOB_SIZE)
 
 if args.filename:
     # Make sure the JSON file exists before we try to load it.
     if not os.path.exists(args.filename):
-        print('Imagegen - ERROR - Could not locate file \'' + args.filename + '\'.')
+        print('Imagegen - ERROR - Could not locate file \'%s\'.' % args.filename)
         exit()
 
     project = Project()
     project.load_json(args.filename)
 
-    job_size = 32   # TODO: Make available as a command line argument
-
     start_time = time.time()
 
     for _, output in project.output.items():
-        image = output.generate_image(job_size)
+        image = output.generate_image(JOB_SIZE)
         if image is not None:
             image.save(output.filename)
             # image.show()
